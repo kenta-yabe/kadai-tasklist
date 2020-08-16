630b99e8939e8b2cfa3de21b8eca2eefa48a3976
kenta.yabe <kenta.yabe.0127@gmail.com>
2020-08-16 07:15:12 +0000

    add user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,29 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
-      @tasks = Task.all.page(params[:page]).per(10)
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).par(10)
+    end
   end
   
   def show
   end
  
   def new
-      @task = Task.new
+      @task = current_user.tasks.build 
   end
 
   def create
-      @task = Task.new(task_params)
-
+       @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に登録されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクを登録できませんでした'
-      render :new
+     # @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'new'
     end
   end
 
@@ -47,13 +50,16 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
